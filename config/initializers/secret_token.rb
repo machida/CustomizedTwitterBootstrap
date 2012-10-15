bbed5edfa0e481e1cbfb8ec971abba4aebee8097
teppei machida <machidanohimitsu@gmail.com>
2012-10-15 14:14:29 +0900

    Add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CustomizedTwitterBootstrap::Application.config.secret_token = '0dce69fe33f4b64d9f2c6b422aa634c1f4a678e8f8ad639504a43eb23de2db3a73ad9f49c4492e476623bcc1c3d4f40a4c38403b88015ea60ab158e4589da0ba'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+CustomizedTwitterBootstrap::Application.routes.draw do
+  root to: 'home#index'
+end
